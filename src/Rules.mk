15949685cbdd0cbca852c1cceff63641e48ea450
Gary Jones <gjones@profoundlogic.com>
2026-03-09 14:53:18 -0400

    Add claims/accident maintenance application
    Add claim01r.rpgle, claim01d.dspf, claimsp.pf, and claims2l.lf source
    files implementing a claims/accident maintenance application with
    subfile list, detail edit, and delete confirmation screens. Update
    Rules.mk with build rules and dependencies. Load 49 sample claim
    records into the CLAIMSP table.
    
    Co-Authored-By: CoderFlow <ai@coderflow.ai>
    Learn more: https://coderflow.ai

--- src/Rules.mk
+++ src/Rules.mk
@@ -3,6 +3,7 @@ hellod.file: hellod.dspf
 menu.file: menu.dspf
 wrkcustd.file: wrkcustd.dspf
 wrkcust1d.file: wrkcust1d.dspf
+claim01d.file: claim01d.dspf
 
 # Rich Display Files
 wrkcustdo.file: wrkcustdo.json
@@ -13,6 +14,10 @@ custp.file: custp.pf
 ordhdrp.file: ordhdrp.pf
 orddtlp.file: orddtlp.pf
 prodp.file: prodp.pf
+claimsp.file: claimsp.pf
+
+# Logical files
+claims2l.file: claims2l.lf | claimsp.file
 
 # Message file and menu
 menu.msgf: menu.msgf
@@ -31,6 +36,9 @@ cust.bnddir: cust.bnddir | custr.srvpgm
 # Programs with service program and copy member dependencies
 wrkcustr.pgm: wrkcustr.rpgle custr_pr.rpgle wrkcustd.file custr.srvpgm | cust.bnddir
 wrkcust1r.pgm: wrkcust1r.rpgle custr_pr.rpgle wrkcust1d.file custr.srvpgm | cust.bnddir
+
+# Claims program
+claim01r.pgm: claim01r.rpgle claim01d.file | claimsp.file claims2l.file
 
 # RPGOA Programs
 wrkcustro.pgm: wrkcustro.rpgle custr_pr.rpgle wrkcustdo.file custr.srvpgm | cust.bnddir
